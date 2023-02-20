e8641651a5050e946b3bafcc2db0375495182f95
Ummyasmen <madinamjs@gmail.com>
2023-02-20 05:27:48 -0800

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main- A c program that prints a line to the standard error
- * return: 1 (success)
-*/
+ *main- A c program that prints a line to the standard error
+ *return: always 1.
+ */
 int main(void)
 {
-	fputs("and that piece of art is useful" - Dora Korpar, 2015-10-19);
+	writes("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdiout);
 return (1);
 }
