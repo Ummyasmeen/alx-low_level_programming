a0c5a5b5592c6485daea28343bb501071b6a9d31
Ummyasmen <madinamjs@gmail.com>
2023-02-20 05:35:23 -0800

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,6 +5,6 @@
  */
 int main(void)
 {
-	writes("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdiout);
+	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdiout);
 return (1);
 }
