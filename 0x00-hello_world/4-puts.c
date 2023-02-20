5394f3359c6dffb4764059b72faebf1d7cb576a8
Ummyasmen <madinamjs@gmail.com>
2023-02-20 00:43:59 -0800

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - A program that prints a line wwith puts function
+ * main- A program that prints a line wwith puts function
  * Return: 0 (success)
 */
 int main(void)
